c0ce467d04b0a15a584f4f21d8aae741d65aad09
RalphAdi <153388309+RalphAdi@users.noreply.github.com>
2024-03-30 13:50:23 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ var backgroundImage, car1_img, car2_img, track;
 var fuelImage, powerCoinImage, lifeImage;
 var obstacle1Image, obstacle2Image;
 var database, gameState;
+var blastImage;
 var form, player, playerCount;
 var allPlayers, car1, car2, fuels, powerCoins, obstacles;
 var cars = [];
@@ -17,6 +18,7 @@ function preload() {
   obstacle1Image = loadImage("./assets/obstacle1.png");
   obstacle2Image = loadImage("./assets/obstacle2.png");
   lifeImage = loadImage("./assets/life.png");
+  blastImage = loadImage("./assets/blast.png");
 }
 
 function setup() {
